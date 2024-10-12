863547c279a1d0ae5b43ba213c189d1b579a92ec
Ben Cooper <cooperben83@gmail.com>
2024-10-12 18:09:59 +0100

    fixed code gen again

--- src/routes/code/+page.svelte
+++ src/routes/code/+page.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
 	import PopupAd from '$lib/components/popupAd.svelte';
+	import { getPBClient } from '$lib/utils/pb.js';
+	import { onMount } from 'svelte';
 
-	export let data;
-	let codeCharacters: string[] = data?.codeString?.split('') ?? [];
+	let codeString: string;
+
+	onMount(async () => {
+		const pb = await getPBClient();
+		// randomly generate a 4 digit code
+		const codeStringToSend = Math.floor(Math.random() * 10000).toString();
+
+		try {
+			// save the code to the database
+			await pb.collection('codes').create({
+				code: codeStringToSend
+			});
+			console.log('saved code:  ', codeString);
+			codeString = codeStringToSend;
+		} catch (e) {
+			console.log('error', e);
+		}
+	});
 </script>
 
 <div class="container mx-auto max-w-xl pt-10 text-red-500">
@@ -11,14 +29,18 @@
 	<h3 class="text-center text-xl">You can now start using the fire extinguisher.</h3>
 
 	<div class="mt-10 flex w-full flex-col gap-4 text-center">
-		<p>Type in the code below to get started.</p>
-		<div class="flex w-full flex-row justify-center gap-2">
-			{#each codeCharacters as codeCharacter}
-				<span class="rounded-lg bg-slate-800 p-4 px-8 text-center font-mono text-4xl"
-					>{codeCharacter}</span
-				>
-			{/each}
-		</div>
+		{#if codeString}
+			<p>Type in the code below to get started.</p>
+			<div class="flex w-full flex-row justify-center gap-2">
+				{#each codeString?.split('') as codeCharacter}
+					<span class="rounded-lg bg-slate-800 p-4 px-8 text-center font-mono text-4xl"
+						>{codeCharacter}</span
+					>
+				{/each}
+			</div>
+		{:else}
+			<p>Loading...</p>
+		{/if}
 	</div>
 </div>
 
